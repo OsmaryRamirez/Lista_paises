e9093d70cc4b50410fbd0e3d34fb09a43ddbfc15
Osmary <Osmary@DESKTOP-8DOM8BM>
2024-03-03 19:58:54 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Lista_País
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cbo_lista_países = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.txt_capital = new System.Windows.Forms.TextBox();
+            this.txt_idioma = new System.Windows.Forms.TextBox();
+            this.txt_población = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_religión = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbo_lista_países
+            // 
+            this.cbo_lista_países.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbo_lista_países.FormattingEnabled = true;
+            this.cbo_lista_países.Items.AddRange(new object[] {
+            "",
+            "Venezuela",
+            "República Dominicana",
+            "Haití",
+            "España",
+            "China",
+            "Corea del Sur",
+            "Corea del Norte",
+            "Estados Unidos",
+            "México",
+            "Canadá",
+            "Perú",
+            "Colombia",
+            "Puerto Rico ",
+            "Argentina"});
+            this.cbo_lista_países.Location = new System.Drawing.Point(201, 90);
+            this.cbo_lista_países.Margin = new System.Windows.Forms.Padding(4);
+            this.cbo_lista_países.Name = "cbo_lista_países";
+            this.cbo_lista_países.Size = new System.Drawing.Size(250, 33);
+            this.cbo_lista_países.TabIndex = 0;
+            this.cbo_lista_países.SelectedIndexChanged += new System.EventHandler(this.cbo_lista_países_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(211, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lista de países";
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Font = new System.Drawing.Font("Microsoft JhengHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.Location = new System.Drawing.Point(632, 35);
+            this.btn_limpiar.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(133, 47);
+            this.btn_limpiar.TabIndex = 3;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Font = new System.Drawing.Font("Microsoft JhengHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(835, 35);
+            this.btn_salir.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(133, 47);
+            this.btn_salir.TabIndex = 4;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // txt_capital
+            // 
+            this.txt_capital.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_capital.Location = new System.Drawing.Point(148, 161);
+            this.txt_capital.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_capital.Name = "txt_capital";
+            this.txt_capital.Size = new System.Drawing.Size(419, 29);
+            this.txt_capital.TabIndex = 5;
+            // 
+            // txt_idioma
+            // 
+            this.txt_idioma.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_idioma.Location = new System.Drawing.Point(148, 280);
+            this.txt_idioma.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_idioma.Multiline = true;
+            this.txt_idioma.Name = "txt_idioma";
+            this.txt_idioma.Size = new System.Drawing.Size(419, 31);
+            this.txt_idioma.TabIndex = 6;
+            // 
+            // txt_población
+            // 
+            this.txt_población.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_población.Location = new System.Drawing.Point(148, 218);
+            this.txt_población.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_población.Name = "txt_población";
+            this.txt_población.Size = new System.Drawing.Size(412, 29);
+            this.txt_población.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 154);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 31);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Capital";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(11, 211);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 31);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Población";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft JhengHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 267);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 31);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Idioma";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft JhengHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(29, 328);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 31);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Religión";
+            // 
+            // txt_religión
+            // 
+            this.txt_religión.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_religión.Location = new System.Drawing.Point(148, 334);
+            this.txt_religión.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_religión.Multiline = true;
+            this.txt_religión.Name = "txt_religión";
+            this.txt_religión.Size = new System.Drawing.Size(412, 199);
+            this.txt_religión.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(632, 154);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(321, 315);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(1001, 592);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txt_religión);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_población);
+            this.Controls.Add(this.txt_idioma);
+            this.Controls.Add(this.txt_capital);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbo_lista_países);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbo_lista_países;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.TextBox txt_capital;
+        private System.Windows.Forms.TextBox txt_idioma;
+        private System.Windows.Forms.TextBox txt_población;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_religión;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista_País
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cbo_lista_países_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string pais = cbo_lista_países.SelectedItem.ToString();
+
+            // Mostrar la información del país en los TextBox
+            switch (pais)
+            {
+                case "Estados Unidos":
+                    txt_capital.Text = "Washington D.C.";
+                    txt_población.Text = "331,002,651";
+                    txt_idioma.Text = "Inglés";
+                    txt_religión.Text = "Cristianismo";
+                    break;
+                case "China":
+                    txt_capital.Text = "Pekín";
+                    txt_población.Text = "1,439,323,776";
+                    txt_idioma.Text = "Chino Mandarín";
+                    txt_religión.Text = "Ateísmo / Religiones tradicionales chinas";
+                    break;
+                case "República Dominicana":
+                    txt_capital.Text = "Santo Domingo";
+                    txt_población.Text = "10.9 millones de habitantes";
+                    txt_idioma.Text = "Español";
+                    txt_religión.Text = "Cristianismo";
+                    break;
+                case "Haití":
+                    txt_capital.Text = "Pueto Príncipe";
+                    txt_población.Text = "11 millones de habitantes";
+                    txt_idioma.Text = "Francés";
+                    txt_religión.Text = "Cristianismo";
+                    break;
+                case "España":
+                    txt_capital.Text = "Madrid";
+                    txt_población.Text = " 46.9 millones de habitantes";
+                    txt_idioma.Text = " Español";
+                    txt_religión.Text = "El catolicismo";
+                    break;
+                case "Corea del Sur":
+                    txt_capital.Text = "Seúl.";
+                    txt_población.Text = "51 millones de habitantes.";
+                    txt_idioma.Text = " Es el coreano, pero en Corea del Sur se utiliza una forma ligeramente diferente del idioma, conocida como Hangeul.";
+                    txt_religión.Text = "Budismo y el cristianismo";
+                    break;
+                case "Corea del Norte":
+                    txt_capital.Text = "Pyongyang. ";
+                    txt_población.Text = "25 millones de habitantes.";
+                    txt_idioma.Text = "El idioma oficial es el coreano, conocido como Chosŏn'gŭl en Corea del Norte.";
+                    txt_religión.Text = "Ateísmo de estado.";
+                    break;
+                case "Venezuela":
+                    txt_capital.Text = "Caracas";
+                    txt_población.Text = "28,5 millones de habitantes.5";
+                    txt_idioma.Text = "Hindi, Inglés";
+                    txt_religión.Text = "Español.";
+                    break;
+                case "México":
+                    txt_capital.Text = "Ciudad de México";
+                    txt_población.Text = "1,380,004,385";
+                    txt_idioma.Text = "Español";
+                    txt_religión.Text = "Catolicismo romano";
+                    break;
+                case "Canadá":
+                    txt_capital.Text = "Ottawa";
+                    txt_población.Text = "38 millones de habitantes";
+                    txt_idioma.Text = " Inglés y el francés";
+                    txt_religión.Text = "Cristianismo, el islam, el judaísmo, el hinduismo y el sijismo";
+                    break;
+                case "Perú":
+                    txt_capital.Text = "Lima";
+                    txt_población.Text = "33 millones de habitantes";
+                    txt_idioma.Text = "Hindi, Inglés";
+                    txt_religión.Text = "La religión en Perú tradicionalmente está relacionada al sincretismo religioso originado del catolicismo" +
+                        "con la antigua religión incaica tras la Conquista española.Sin embargo en los últimos 32 años se han desarrollado" +
+                        "considerablemente iglesias protestantes de distintas denominaciones en los sectores populares.";
+                    break;
+                case "Colombia":
+                    txt_capital.Text = "Bogotá";
+                    txt_población.Text = "51 millones de habitantes";
+                    txt_idioma.Text = "Español";
+                    txt_religión.Text = "Colombia es un país laico, lo que significa que no tiene una religión oficial establecida por el gobierno. " +
+                        "Sin embargo, la religión predominante en Colombia es el cristianismo, específicamente el catolicismo romano";
+                    break;
+                case "Argentina":
+                    txt_capital.Text = " Buenos Aires";
+                    txt_población.Text = " 45 millones de habitantes";
+                    txt_idioma.Text = "Español";
+                    txt_religión.Text = "En Argentina, la religión predominante es el cristianismo, " +
+                        "específicamente el catolicismo romano. ";
+                    break;
+
+                default:
+                    MessageBox.Show("Seleccione un país válido.");
+                    break;
+
+            }
+
+       
+            
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            // Limpiar los cuadros de texto
+            txt_capital.Clear();
+            txt_población.Clear();
+            txt_idioma.Clear();
+            txt_religión.Clear();
+            
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
